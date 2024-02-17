947d30b5cad9af66bd7e1fd77cff6da38729b62d
Noonexyz1 <aries-d2github@hotmail.com>
2024-02-17 16:34:40 -0400

    Patron Proxy mejorado

--- Patrones Estructurales/Proxy/src/main/java/org/example/Main.java
+++ Patrones Estructurales/Proxy/src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.example.proxy.ServicioMensajeImpl;
 import org.example.proxy.ServicioMensajeProxy;
 import org.example.proxy.interfaces.ServicioMensaje;
 
 public class Main {
     public static void main(String[] args) {
+        //Instanciacion normal
+        ServicioMensaje sm1 = new ServicioMensajeImpl();
+        System.out.println(sm1.mensaje("Juan"));;
 
+        /*Me piden que ejecute una accion antes o despues de llamar al metodo en concreto*/
         ServicioMensaje sm = new ServicioMensajeProxy();
-        System.out.println(sm.mensaje("pepito"));
+        System.out.println(sm.mensaje("Juan"));
 
     }
 
